8158b6d83f88cf0ab02b55cfc984a68a0afa8696
Quoc Tran <quoc.tran@saigontechnology.com>
2024-03-08 14:42:30 +0700

    Added more use-cases

---
+++ GrpcGreeterClient/Handler.cs
@@ -0,0 +1,14 @@
+﻿namespace GrpcGreeterClient
+{
+    class CustomHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            response.Headers.Remove("grpc-status");
+
+            return response;
+        }
+    }
+}
