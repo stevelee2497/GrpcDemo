85a1d6eca86a4f8b8733f549264c5c8302a96048
Quoc Tran <quoc.tran@saigontechnology.com>
2024-03-17 13:35:37 +0700

    Updated project structure

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Grpc.Net.Client;
-using GrpcGreeterClient;
 using System.Text.Json;
 
 // The port number must match the port of the gRPC server.
@@ -12,14 +11,6 @@ var userId = "user - 1";
 var todoClient = new Todo.TodoClient(channel); //etcd
 var todoItems = await todoClient.GetTodoItemsAsync(new TodoItemsRequest { UserId = userId });
 Console.WriteLine($"Todo list for user: {userId}, items: {JsonSerializer.Serialize(todoItems)}");
-
-using var teamContactChannel = GrpcChannel.ForAddress("http://localhost:8998", new GrpcChannelOptions
-{
-    HttpHandler = new CustomHandler()
-});
-var teamContactClient = new TeamContacts.TeamContactsClient(teamContactChannel);
-var contacts = await teamContactClient.SearchAsync(new SearchTeamContactsRequest { TeamId = "cd0a92a1-444e-4178-9648-a2db757f0713" });
-Console.WriteLine(JsonSerializer.Serialize(contacts));
 
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
